b2a55cb6aa4358391a824e29874fe3a81c20da3c
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2025-09-22 17:43:40 -0300

    Charge balance to mint

--- src/Ingressos.sol
+++ src/Ingressos.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.13;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Ingressos is ERC721 {
-    uint256 public number;
+    uint256 private _totalTickets;
+    uint256 private immutable MAX_TICKETS;
 
-    constructor() ERC721("Ingressos", "ING") {}
+    constructor(uint256 maxTickets) ERC721("Tickets", "TIC") {
+        MAX_TICKETS = maxTickets;
+    }
 
-    function mint(uint256 tokenId) public {
+    function mint(uint256 tokenId) external payable {
+        require(msg.value == 0.1 ether, "Insufficient funds");
+        uint256 totalTickets = _totalTickets + 1;
+        require(totalTickets < MAX_TICKETS);
+        _totalTickets = totalTickets;
         _mint(msg.sender, tokenId);
     }
 }
--- test/Ingressos.t.sol
+++ test/Ingressos.t.sol
@@ -8,19 +8,27 @@ contract IngressosTest is Test {
     Ingressos public ingressos;
 
     address private constant SENDER = 0x0A3bF3197EF42b596460C6678983A7ef62cf5207;
+    uint256 private constant MAX_TICKETS = 100;
+    uint256 private constant NFT_PRICE = 0.1 ether;
+
+    constructor() {
+        vm.deal(SENDER, 100 ether);
+    }
 
     function setUp() public {
-        ingressos = new Ingressos();
+        ingressos = new Ingressos(MAX_TICKETS);
     }
 
     function test_mint() public {
         vm.startPrank(SENDER);
         uint256 nft = 1;
 
+        assertEq(address(ingressos).balance, 0);
         vm.expectRevert();
         ingressos.ownerOf(nft);
 
-        ingressos.mint(1);
+        ingressos.mint{value: NFT_PRICE}(1);
         assertEq(ingressos.ownerOf(nft), SENDER);
+        assertEq(address(ingressos).balance, NFT_PRICE);
     }
 }
